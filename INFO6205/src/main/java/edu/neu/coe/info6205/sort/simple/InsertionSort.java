b700df2cef930e357e1434b2d8986e7237948879
Mahalakshmi Arunachalam <arunachalam.m@husky.neu.edu>
2018-07-09 16:46:52 -0400

    Final assignment submission

---
+++ INFO6205/src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
@@ -0,0 +1,20 @@
+package edu.neu.coe.info6205.sort.simple;
+
+import static edu.neu.coe.info6205.sort.simple.Helper.*;
+
+public class InsertionSort<X extends Comparable<X>> implements Sort<X> {
+    @Override
+    public void sort(X[] xs, int from, int to) {
+        // TODO implement insertionSort
+    	 for(int i=0; i<to; i++)
+         {
+             for(int j=i; j>0 && Helper.less(xs[j], xs[j-1]); j--)
+             {
+                Helper.swap(xs, j, j-1);
+             }
+         }
+    }
+    
+
+
+}
